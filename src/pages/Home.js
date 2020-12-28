8afb1d85485455f12fc77624874f5a62e318923b
Azhari Marzan <azhari.marzan@gmail.com>
2020-12-28 11:23:04 +0700

    + pages

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = (props) => {
+    return (
+        <div>
+            <h1 className="text-center">React Playground</h1>
+        </div>
+    )
+}
+
+export default Home
