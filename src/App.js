e7f6a94ad0dad85ea844ffc54a986166495ea9e8
Azhari Marzan <azhari.marzan@gmail.com>
2020-12-28 11:06:47 +0700

    ^ pages

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
-import axios from "axios";
-import { API_KEY } from './env'
 import "./App.css";
+import Lazyloading from './pages/LazyLoading'
 
 function App() {
-    const url =
-        `https://api.unsplash.com/photos?client_id=${API_KEY}`;
-    const [images, setImages] = useState([]);
-    const getImages = async () => {
-        const response = await axios.get(url);
-        setImages(response.data);
-    };
-    useEffect(() => {
-        getImages();
-    }, []);
-
-    if (!images) {
-        return <h1>Loading...</h1>;
-    }
+    
     return (
-        <div className="App">
-            {images.map((image, index) => {
-                return (
-                    <LazyLoadImage
-						effect="blur"
-                        src={image.urls.regular}
-                        alt={image.alt_description}
-						key={index}
-                    />
-                );
-            })}
-        </div>
+        <Lazyloading />
     );
 }
 
